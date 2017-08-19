51c789774e302393caca0a0e3b4b3b07e81e8901
vivekdeosarkar <viveksdevsarkar@gmail.com>
2017-08-19 17:47:18 +0530

    Update CalculationService.java

--- src/main/java/com/vodafone/assignment/services/CalculationService.java
+++ src/main/java/com/vodafone/assignment/services/CalculationService.java
@@ -1,9 +1,11 @@
 package com.vodafone.assignment.services;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import org.springframework.stereotype.Service;
 
@@ -14,47 +16,39 @@ import com.vodafone.assignment.dto.Voucher;
 @Service
 public class CalculationService {
 	public Collection<Reward> getRewardsByPerson(List<Voucher> vouchers, List<Credit> credits) {
-		Map<UserInfo, Reward> mapOfRewards = populateMapOfRewardsFromVouchers(vouchers);
-		populateCreditInfoToRewards(mapOfRewards, credits);
+		Map<UserInfo, Reward> mapOfRewards =new HashMap<>();
+		vouchers.parallelStream().forEach(voucher-> populateMapOfRewardsFromVouchers(voucher,mapOfRewards));		
+		credits.parallelStream().forEach(credit-> populateCreditInfoToRewards(credit,mapOfRewards));
 		return mapOfRewards.values();
-	}
+	}	
 	
-	private Map<UserInfo, Reward> populateMapOfRewardsFromVouchers(List<Voucher> vouchers) {
-		final Map<UserInfo, Reward> rewardsMap = new HashMap<>();
-		for(final Voucher voucher : vouchers) {
-			UserInfo existingVoucherUser = new UserInfo(voucher.getFirstName(), voucher.getLastName());
-			
-			Reward reward = rewardsMap.get(existingVoucherUser);
-			if(null != reward) {
-				if(!reward.getVoucher().contains(voucher.getVoucherNumber())) {
-					reward.getVoucher().add(voucher.getVoucherNumber());
-					
-				}
-			} else {
-				final Reward newReward =  new Reward(voucher.getFirstName(), voucher.getLastName());
-				newReward.getVoucher().add(voucher.getVoucherNumber());
-				rewardsMap.put(existingVoucherUser, newReward);
+	private Reward populateMapOfRewardsFromVouchers(Voucher voucher, Map<UserInfo, Reward> rewardsMap) {
+		UserInfo existingVoucherUser = new UserInfo(voucher.getFirstName(), voucher.getLastName());		
+		Reward reward = rewardsMap.get(existingVoucherUser);
+		if(null != reward) {
+			if(!reward.getVoucher().contains(voucher.getVoucherNumber())) {
+				reward.getVoucher().add(voucher.getVoucherNumber());				
 			}
+		} else {
+			final Reward newReward =  new Reward(voucher.getFirstName(), voucher.getLastName());
+			newReward.getVoucher().add(voucher.getVoucherNumber());
+			rewardsMap.put(existingVoucherUser, newReward);
 		}
-		// TODO Auto-generated method stub
-		return rewardsMap;
+		return reward;
 	}
-	
-	private void populateCreditInfoToRewards(final Map<UserInfo, Reward> rewardsMap, List<Credit> credits) {
-		for(final Credit credit : credits) {
-			final UserInfo existingCreditUserInfo = new UserInfo(credit.getFirstName(), credit.getLastName());
-			Reward reward = rewardsMap.get(existingCreditUserInfo);
-			if(null != reward) {
-				if(!reward.getCredit().contains(credit.getCreditId())) {
-					reward.getCredit().add(credit.getCreditId());
-				}
-				
-			} else {
-				final Reward newReward =  new Reward(credit.getFirstName(), credit.getLastName());
-				newReward.getCredit().add(credit.getCreditId());
-				rewardsMap.put(existingCreditUserInfo, newReward);
-			}
-			
+
+	private Reward populateCreditInfoToRewards(Credit credit, Map<UserInfo, Reward> rewardsMap) {
+		final UserInfo existingCreditUserInfo = new UserInfo(credit.getFirstName(), credit.getLastName());
+		Reward reward = rewardsMap.get(existingCreditUserInfo);
+		if(null != reward) {
+			if(!reward.getCredit().contains(credit.getCreditId())) {
+				reward.getCredit().add(credit.getCreditId());
+			}			
+		} else {
+			final Reward newReward =  new Reward(credit.getFirstName(), credit.getLastName());
+			newReward.getCredit().add(credit.getCreditId());
+			rewardsMap.put(existingCreditUserInfo, newReward);
 		}
-	}
+		return reward;
+	}	
 }
